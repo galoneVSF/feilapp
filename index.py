5ab337e95317049414e2c4fd9a6a4d5bc17e93dc
Afeil <Afeil@1NEH0193>
2021-06-15 09:36:48 -0300

    novedades

--- index.py
+++ index.py
@@ -11,8 +11,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Veremos que pasa, aguante Aka!!!! <a href='dataMayor/RESUMEN_ContextoActualEcommerce.xlsx' >Repo</a> <a href='RESUMEN_ContextoActualEcommerce.rar' >RepoRar</a>"
+    return """
+        <html>
+            <body>
+                <h1>Transform a file demo</h1>
+
+                <form action="/transform" method="post" enctype="multipart/form-data">
+                    <input type="file" name="data_file" />
+                    <input type="submit" />
+                </form>
+            </body>
+        </html>
+    """
+
+#    return "Veremos que pasa, aguante Aka!!!! <a href='dataMayor/RESUMEN_ContextoActualEcommerce.xlsx' >Repo</a> <a href='RESUMEN_ContextoActualEcommerce.rar' >RepoRar</a>"
 
 @app.route('/data')
 def data():
     return dataJS
+    
+    
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    request_file = request.files['data_file']
+    if not request_file:
+        return "No file"
+
+    file_contents = request_file.stream.read().decode("utf-8")
+
+    result = transform(file_contents)
+
+    response = make_response(result)
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
